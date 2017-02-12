d265b799034e4050ce6f26e232ec57c7ab2829d5
ElizavetaTomaeva <lizvladt@mail.ru>
2017-02-12 18:50:30 +0300

    Add files via upload

--- unpack.py
+++ unpack.py
@@ -1,23 +1,26 @@
 from pyunpack import Archive
-import patoolib
 import os
-from os.path import splitext, basename, expanduser, exists, dirname
+from os.path import splitext, basename, expanduser, exists
 import shutil
-from rarfile import RarFile
+
 
 def extract(link):  # запись содержимого архива во временную директорию
-    temp = os.path.join(expanduser('~/.current/'), splitext(basename(link))[0])
+    temp = os.path.join(expanduser('~/.current/'), splitext(basename(link))[0])  # '~/.current/'
     if exists(temp):
         shutil.rmtree(temp)
     os.makedirs(temp)
-    # temp2 = 'files'
     Archive(link).extractall(temp)
-    # rarfile.RarFile(link).extractall(temp)
-    # patoolib.extract_archive(link, temp)
     return os.path.abspath(temp)
+
 
 def all_pages(link):  # список всех страниц во временной директории
     pages = []
     for root, dirs, files in os.walk(link):
         pages.extend([os.path.join(root, fname) for fname in sorted(files)])
     return pages
+
+
+def get_book_name(link):  # имя каталога
+    return splitext(basename(link))[0]
+
+
--- window.py
+++ window.py
@@ -1,12 +1,11 @@
-from PyQt5.QtWidgets import QDialog, QMainWindow, QTextEdit, QAction, QFileDialog
-from PyQt5.QtWidgets import (QMainWindow, QWidget, QTextEdit,
-                             QAction, QFileDialog, QApplication, QScrollArea, QLabel, QToolBar, QVBoxLayout,
-                             QDockWidget, QComboBox, QListView, QListWidget, QGraphicsAnchorLayout)
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QMainWindow, QAction, QFileDialog,
+                             QApplication, QScrollArea, QLabel,
+                             QVBoxLayout,QComboBox, QCheckBox)
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
 import unpack
 import sys
-from os.path import exists
+
 
 class Example(QMainWindow):
     def __init__(self):
@@ -15,8 +14,9 @@ class Example(QMainWindow):
         self.__init_signals()
 
     def initUI(self):
-        self.setFixedSize(1350, 695)
-        self.setGeometry(10, 30, 1000, 300)
+        # self.showFullScreen()
+        self.setFixedSize(1360, 695)
+        # self.setGeometry(10, 30, 1000, 300)
         self.setWindowTitle("Comic Book Reader")
 
         self.background = QVBoxLayout(self)
@@ -29,6 +29,7 @@ class Example(QMainWindow):
 
         self.imageArea = QLabel()
         self.scrollArea.setWidget(self.imageArea)
+        self.scrollArea.hide()
 
         self.statusBar()
 
@@ -37,27 +38,35 @@ class Example(QMainWindow):
         self.exit.setStatusTip('Exit application')
 
         self.open = QAction(QIcon('./images/open.png'), 'Open', self)
+        self.open.setShortcut('Ctrl+O')
         self.open.setStatusTip('Open file')
 
         self.backward = QAction(QIcon('./images/backward.png'), 'Backward', self)
         self.backward.setStatusTip('Previous page')
+        self.backward.setEnabled(False)
 
         self.forward = QAction(QIcon('./images/forward.png'), 'Forward', self)
         self.forward.setStatusTip('Next page')
+        self.forward.setEnabled(False)
 
-        self.toolbar = self.addToolBar('Tools', )
-        # self.toolbar.setGeometry(50, 100, self.height(), self.width())
+        self.toolbar = self.addToolBar('Tools')
         self.toolbar.addAction(self.open)
         self.toolbar.addAction(self.backward)
         self.toolbar.addAction(self.forward)
         self.toolbar.addAction(self.exit)
-
+        self.toolbar.setStyleSheet("QToolBar {border-color: none}")
         self.closeFile = QAction('Exit', self)
         self.open.setStatusTip('Open file')
 
+        self.sizeCheck = QCheckBox(self)
+        self.sizeCheck.setText('Cover')
+        self.sizeCheck.setGeometry(1100, 10, 100, 20)
+        self.sizeCheck.hide()
+
         self.pageBox = QComboBox(self)
+        self.pageBox.setFocusPolicy(Qt.NoFocus)
         self.pageBox.setGeometry(1220, 10, 100, 20)
-        self.pageBox.setStatusTip("Page")
+        self.pageBox.setStatusTip('Page')
         self.pageBox.hide()
 
         self.show()
@@ -66,39 +75,66 @@ class Example(QMainWindow):
         self.backward.triggered.connect(self.previous_page)
         self.forward.triggered.connect(self.next_page)
         self.open.triggered.connect(self.show_dialog)
+        # self.startOpenBtn.clicked.connect(self.show_dialog)
+        self.sizeCheck.stateChanged.connect(self.change_size)
         self.closeFile.triggered.connect(self.close)
         self.pageBox.currentIndexChanged.connect(self.open_page)
         self.exit.triggered.connect(self.close)
 
     def show_dialog(self):  # диалог открытия файла
-        self.bookfile = QFileDialog.getOpenFileName(self, 'Open file', '/home', "Comic Book Archive files (*.cbz *.cbr)")[0]
+        self.bookfile = QFileDialog.getOpenFileName(self, 'Open file', '/home',
+                                                    "Comic Book Archive files (*.cbz *.cbr)")[0]
         if self.bookfile:
-            count = 0
+            self.scrollArea.show()
+            self.backward.setEnabled(True)
+            self.forward.setEnabled(True)
             self.pageBox.show()
-            for page_num in unpack.all_pages(unpack.extract(self.bookfile)):  # страницы в выпадающем списке
-                self.pageBox.addItem(str(count))
-                count += 1
+            self.sizeCheck.show()
+            self.setWindowTitle("Comic Book Reader - " + unpack.get_book_name(self.bookfile))
+            if self.pageBox not in range(0):
+                self.pageBox.clear()
+            self.get_page_num()
         else:
             return
 
+    def get_page_num(self):   # добавление номеров страниц в выпадающий список
+        count = 0
+        for page_num in unpack.all_pages(unpack.extract(self.bookfile)):
+            self.pageBox.addItem(str(count))
+            count += 1
+
     def open_page(self):  # загрузка страницы по номеру из выпадающего списка
-        page = unpack.all_pages(unpack.extract(self.bookfile))[self.get_combo_box_index()]
-        self.myPixmap = QPixmap(page).scaledToWidth(self.width())
-        self.imageArea.setPixmap(self.myPixmap)
+        self.page = unpack.all_pages(unpack.extract(self.bookfile))[self.get_combo_box_index()]
+        if self.get_combo_box_index() == 0:
+            self.load_cover()
+        else:
+            self.load_fullscreen()
 
     def get_combo_box_index(self):  # индекс строки в выпадающем списке
         return self.pageBox.currentIndex()
 
-    # def load_first_page(self):  # загрузка первой страницы при открытии папки
-    #     picture = get_files.all_pages(get_files.extract(self.bookfile))[0]
-    #     self.myPixmap = QPixmap(picture)
-    #     self.imageArea.setPixmap(self.myPixmap)
-
-    def previous_page(self, new_index):  # пролистывание назад
+    def previous_page(self):  # пролистывание назад
         return self.pageBox.setCurrentIndex(self.get_combo_box_index()-1)
 
-    def next_page(self): # пролистывание вперед
+    def next_page(self):  # пролистывание вперед
         return self.pageBox.setCurrentIndex(self.get_combo_box_index()+1)
+
+    def load_fullscreen(self):  # отображение по ширине окна
+        self.myPixmap = QPixmap(self.page).scaledToWidth(self.width())
+        self.imageArea.setPixmap(self.myPixmap)
+        self.sizeCheck.setChecked(False)
+
+    def load_cover(self):  # отображение по высоте окна и по центру
+        self.myPixmap = QPixmap(self.page).scaledToHeight(self.height())
+        self.imageArea.setPixmap(self.myPixmap)
+        self.imageArea.setAlignment(Qt.AlignHCenter)
+        self.sizeCheck.setChecked(True)
+
+    def change_size(self):  # изменение формата отображения
+        if self.sizeCheck.isChecked():
+            self.load_cover()
+        else:
+            self.load_fullscreen()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
